2e675fa964d8e20a081fc3cf6e84969bac801b17
Rodrigo Eliseu <rodelrod@hotmail.com>
2020-12-23 00:29:03 +0100

    fix: IDs for links were not being generated
    I expect org-store-link to just use IDs. The =create-if-interactive=
    option will hopefully prevent the IDs that were showing up every time I
    did a org-capture.

--- config.el
+++ config.el
@@ -216,7 +216,7 @@ Assumes millisecond timestamps."
         org-indirect-buffer-display 'new-frame
         org-list-indent-offset 1                      ; indent plain lists with 3 spaces
         tab-width 3
-        org-id-link-to-org-use-id 'use-existing)
+        org-id-link-to-org-use-id 'create-if-interactive)
 
   ;; Use the fold prefix `z' for outline navigation
   ;; and recover `gk' and `gj' for visual line up and down
