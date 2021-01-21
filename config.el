057a537626e5145609448fa0c84079b754630982
Rodrigo Eliseu <rodelrod@hotmail.com>
2021-01-22 00:38:17 +0100

    boyscout: Reorder org variables alphabetically
    I'm starting to have too many of them

--- config.el
+++ config.el
@@ -196,17 +196,18 @@ Assumes millisecond timestamps."
                                doom-disable-line-numbers-h))
   :config
   (setq org-archive-location "%s_archive::datetree/"
-        org-startup-folded 'content
         org-ellipsis " ▶"
-        org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "WAITING(w@)" "SOMEDAY(s)" "|" "DONE(d)" "CANCELLED(c@)"))
         org-enforce-todo-dependencies t
-        org-log-done t                                ; Add CLOSED timestamp when todo is done
-        org-log-into-drawer t                         ; Put log notes (C-c C-z) and state changes in LOGBOOK drawer.
+        org-id-link-to-org-use-id 'create-if-interactive
         org-indirect-buffer-display 'new-frame
         org-list-indent-offset 1                      ; indent plain lists with 3 spaces
+        org-log-done t                                ; Add CLOSED timestamp when todo is done
+        org-log-into-drawer t                         ; Put log notes (C-c C-z) and state changes in LOGBOOK drawer.
+        org-plantuml-jar-path  "/usr/share/plantuml/plantuml.jar"
+        org-startup-folded 'content
+        org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "WAITING(w@)" "SOMEDAY(s)" "|" "DONE(d)" "CANCELLED(c@)"))
         tab-width 3
-        org-id-link-to-org-use-id 'create-if-interactive
-        org-plantuml-jar-path  "/usr/share/plantuml/plantuml.jar")
+        )
 
   ;; Use the fold prefix `z' for outline navigation
   ;; and recover `gk' and `gj' for visual line up and down
