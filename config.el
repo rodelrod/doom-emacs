553f69458ab236a088a7f60d2b957c333fda333e
Rodrigo Eliseu <rodelrod@hotmail.com>
2021-12-12 02:14:59 +0100

    Give up on setting org-id store behavior
    I would like to change to use-existing, but it's being overridden by org-roam.

--- config.el
+++ config.el
@@ -192,7 +192,6 @@ Assumes millisecond timestamps."
         org-download-image-org-width 400
         org-ellipsis " ▶"
         org-enforce-todo-dependencies t
-        org-id-link-to-org-use-id 'create-if-interactive
         org-list-indent-offset 1                      ; indent plain lists with 3 spaces
         org-log-done t                                ; Add CLOSED timestamp when todo is done
         org-log-into-drawer t                         ; Put log notes (C-c C-z) and state changes in LOGBOOK drawer.
