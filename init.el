2efcb964ddd5735fa1aa764f883d47ec1dedd6cd
Rodrigo Eliseu <rodelrod@hotmail.com>
2026-04-29 15:59:20 +0100

    latex: add tab completion

--- init.el
+++ init.el
@@ -137,7 +137,7 @@
        json
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       latex             ; writing papers in Emacs has never been so fun
+       (latex +cdlatex)  ; writing papers in Emacs has never been so fun
        ;;lean
        ;;factor
        ;;ledger            ; an accounting system in Emacs
